6b11b6c2fbcfc979d23a33e483dfe72f38ba473a
AppleStein <pianonono1129@gmail.com>
2022-04-23 20:57:26 +0900

    偶数奇数判定

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, request
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def odd_even():
-    return """
-    以下に整数を入力...
-    <form action="/" method="POST">
-    <input name="num"></input>
-    </from>"""
+    if request.method == "GET":
+        return """
+        以下に整数を入力...
+        <form action="/" method="POST">
+        <input name="num"></input>
+        </from>"""
+    else:
+        return """
+        {}は{}
+        <form action="/" method="POST">
+        <input name="num"></input>
+        </form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"])%2])
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
